76f3d09046ba474374b45063578ffc6d15fa85e7
Nasir Hafeez <38311694+nasirhafeez@users.noreply.github.com>
2022-09-06 12:04:43 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+header( "Content-type: application/json" );
+
+$jsonData = [
+  "captive" => true,
+  "user-portal-url" => 'https://api.nasirhafeez.com',
+  "venue-info-url" => 'https://api.nasirhafeez.com',
+  "seconds-remaining" => 63200,
+  "can-extend-session" => true
+];
+
+echo json_encode($jsonData, JSON_PRETTY_PRINT);
