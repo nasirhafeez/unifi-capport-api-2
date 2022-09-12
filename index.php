1c553d6aa79cb8cf9e26fd428b6987fb367d68fa
Nasir Hafeez <nasirhafeez2002@gmail.com>
2022-09-12 11:34:14 +0500

    .

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ header( "Content-type: application/json" );
 
 $jsonData = [
   "captive" => true,
-  "user-portal-url" => "https://www.google.com",
+  "user-portal-url" => "https://capport.nasirhafeez.com/guest/s/jp69ndw2",
   "venue-info-url" => "https://apiserver.nasirhafeez.com",
   "seconds-remaining" => 63200,
   "can-extend-session" => true
