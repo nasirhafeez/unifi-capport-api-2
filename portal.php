b38f9ed70caed5a5f9c7db9541aff0d73d3f647e
Nasir Hafeez <nasirhafeez2002@gmail.com>
2022-09-12 16:07:02 +0500

    .

---
+++ portal.php
@@ -0,0 +1,17 @@
+<?php
+
+print_r($_REQUEST);
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>WiFi Portal</title>
+  <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+</head>
+<body>
+<p>User Portal Test URL</p>
+</body>
+</html>
