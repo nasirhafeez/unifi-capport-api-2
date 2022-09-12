543160a218dac18d59c631b67c850c2bf02e3645
Nasir Hafeez <nasirhafeez2002@gmail.com>
2022-09-12 16:13:04 +0500

    .

--- portal.php
+++ portal.php
@@ -4,15 +4,15 @@
 header('Location: http://captive.apple.com/hotspot-detect.html')
 
 ?>
-
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>WiFi Portal</title>
-  <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
-</head>
-<body>
-<p>User Portal Test URL</p>
-</body>
-</html>
+<!---->
+<!--<!doctype html>-->
+<!--<html lang="en">-->
+<!--<head>-->
+<!--  <meta charset="utf-8">-->
+<!--  <title>WiFi Portal</title>-->
+<!--  <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">-->
+<!--</head>-->
+<!--<body>-->
+<!--<p>User Portal Test URL</p>-->
+<!--</body>-->
+<!--</html>-->
