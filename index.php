942390f55a171edb8da677cf9e6f2c5f5dc46448
Nasir Hafeez <nasirhafeez2002@gmail.com>
2022-09-12 16:00:15 +0500

    .

--- index.php
+++ index.php
@@ -1,20 +1,18 @@
 <?php
 
-print_r($_REQUEST);
+header( "Content-type: application/captive+json" );
 
-//header( "Content-type: application/captive+json" );
-//
-//$jsonData = [
-////  "captive" => true,
-////  "user-portal-url" => "https://capport.nasirhafeez.com/guest/s/jp69ndw2/index.php",
-////  "venue-info-url" => "https://apiserver.nasirhafeez.com",
-////  "seconds-remaining" => 63200,
-////  "can-extend-session" => true
+$jsonData = [
 //  "captive" => true,
-//	"user-portal-url" => "https://www.nasirhafeez.com",
-//	"venue-info-url" => "https://capport.net/r?url=https://capport.net",
-//	"seconds-remaining" => 57513,
-//	"can-extend-session" => true
-//];
-//
-//echo json_encode($jsonData, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+//  "user-portal-url" => "https://capport.nasirhafeez.com/guest/s/jp69ndw2/index.php",
+//  "venue-info-url" => "https://apiserver.nasirhafeez.com",
+//  "seconds-remaining" => 63200,
+//  "can-extend-session" => true
+  "captive" => true,
+	"user-portal-url" => "https://www.nasirhafeez.com",
+	"venue-info-url" => "https://capport.net/r?url=https://capport.net",
+	"seconds-remaining" => 57513,
+	"can-extend-session" => true
+];
+
+echo json_encode($_REQUEST, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
