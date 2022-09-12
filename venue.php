207b9399fd4962fe542c7ce908f2dadefac357a1
Nasir Hafeez <nasirhafeez2002@gmail.com>
2022-09-12 15:34:20 +0500

    .

--- venue.php
+++ venue.php
@@ -1,6 +1,6 @@
 <?php
 
-print_r($_REQUEST);
+//print_r($_REQUEST);
 
 ?>
 
@@ -10,9 +10,8 @@ print_r($_REQUEST);
   <meta charset="utf-8">
   <title>WiFi Portal</title>
   <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
-  <meta http-equiv="refresh" content="5;url=https://www.google.com" />
 </head>
 <body>
-<p>Venue Test!</p>
+<p>Venue Info Test URL</p>
 </body>
 </html>
