4ed15f454103c295f8d9e82f5de006349d196952
Nasir Hafeez <nasirhafeez2002@gmail.com>
2022-09-12 15:38:10 +0500

    .

--- venue.php
+++ venue.php
@@ -12,6 +12,6 @@
   <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 </head>
 <body>
-<p>Venue Info Test URL!</p>
+<p>Venue Info Test URL</p>
 </body>
 </html>
