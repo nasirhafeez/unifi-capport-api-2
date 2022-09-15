b7520b8622543e65b284ad64bd02fd33aebbdbfc
Nasir Hafeez <nasirhafeez2002@gmail.com>
2022-09-15 09:53:04 +0500

    .

--- index.php
+++ index.php
@@ -10,9 +10,7 @@ $jsonData = [
 //  "can-extend-session" => true
   "captive" => false,
 	"user-portal-url" => "https://apiserver.nasirhafeez.com/portal.php",
-	"venue-info-url" => "https://apiserver.nasirhafeez.com/venue.php",
-	"seconds-remaining" => 57513,
-	"can-extend-session" => true
+	"venue-info-url" => "https://apiserver.nasirhafeez.com/venue.php"
 ];
 
 echo json_encode($jsonData, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
