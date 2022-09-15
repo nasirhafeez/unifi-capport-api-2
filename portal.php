3a7fbc1755124ceffbd7a590f48b9325b9792587
Nasir Hafeez <nasirhafeez2002@gmail.com>
2022-09-15 17:14:55 +0500

    .

--- portal.php
+++ portal.php
@@ -1,18 +1,3 @@
 <?php
 
-//print_r($_REQUEST);
-header('Location: http://connectivitycheck.gstatic.com/generate_204')
-
-?>
-<!---->
-<!--<!doctype html>-->
-<!--<html lang="en">-->
-<!--<head>-->
-<!--  <meta charset="utf-8">-->
-<!--  <title>WiFi Portal</title>-->
-<!--  <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">-->
-<!--</head>-->
-<!--<body>-->
-<!--<p>User Portal Test URL</p>-->
-<!--</body>-->
-<!--</html>-->
+header('Location: http://connectivitycheck.gstatic.com/generate_204');
