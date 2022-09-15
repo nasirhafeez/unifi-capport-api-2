3c401f59364411372bf00c185731af557b70f674
Nasir Hafeez <nasirhafeez2002@gmail.com>
2022-09-15 18:00:21 +0500

    .

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ header( "Content-type: application/captive+json" );
 $jsonData = [
   "captive" => false,
 	"user-portal-url" => "https://apiserver.nasirhafeez.com/portal.php",
-	"venue-info-url" => "https://apiserver.nasirhafeez.com/venue.php"
+	"venue-info-url" => "https://apiserver.nasirhafeez.com/venue.html"
 ];
 
 echo json_encode($jsonData, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
